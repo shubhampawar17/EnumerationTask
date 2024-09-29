0dc9122730d5ece85adde95fdb069e05e52561e0
shubhampawar17 <sp1180253@gmail.com>
2024-09-29 16:00:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static EnumerationTask.model.Enum;
+
+namespace EnumerationTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var month  in Enum.GetValues(typeof(MonthsOfYear)))
+            {
+                Console.WriteLine($"Value for {month} :{(int)month}");
+            }
+
+            Cricket cricket = new Cricket();
+            //Cricket cricket = Cricket.Batsman;
+
+            switch (cricket)
+            {
+                case Cricket.Batsman:
+                    Console.WriteLine("I am a Batsman");
+                    break;
+                case Cricket.Bowler:
+                    Console.WriteLine("I am a Bowler");
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+
+            }
+            DaysInWeek daysInWeek = new DaysInWeek();
+            Console.WriteLine(daysInWeek == DaysInWeek.Sunday ? "It is a Weekend" : "It is a Weekday");
+        }
+    }
+}
---
+++ model/Enum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationTask.model
+{
+    internal class Enum
+    {
+        public enum MonthsOfYear
+        {
+          January,
+          February,
+          March,
+          April,
+          May,
+          June,
+          July,
+          August,
+          September,
+          October,
+          November,
+          December
+
+        }
+
+        public enum Cricket
+        {
+            Batsman,
+            Bowler,
+            AllRounder
+        }
+
+        public enum DaysInWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+    }
+}
